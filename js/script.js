be72124aefa2ffc66d0e36304ec9bec999c15d85
Dahye <ydh0606@naver.com>
2019-12-08 04:32:01 +0900

    20191207
    First Test

--- js/script.js
+++ js/script.js
@@ -1,232 +1,116 @@
-// Ready
-console.log ("Script In!");
-console.log ("W = " + window.innerWidth, "H = " + window.innerHeight);
+
+// ready
+console.log("Script Js In!")
+console.log (window.innerWidth, window.innerHeight);
 
 
 
 
 
-$(document).ready(function() {
+// onLoad
 
-    // if($.browser.msie) {
-    //     $('body').addClass('ie');
-    // }
+window.onbeforeunload = function() {
 
+    console.log("Reload")
+    scrollTo(0,0);
 
-
-    // Mouse Position Parallax
-
-    var winW = $(window).width(),
-        winH = $(window).height();
-
-    var conW = $('.contents').width(),
-        proW = $('.profile').width();
-
-    var moveSpeed = 0.75;
-    var bigMove = 30, smallMove = 50, minMove = 100, move = 10;
-
-    $('body').mousemove(function(e) {
-        //console.log(e.pageX, e.pageY);
-
-        // var posX = ((e.pageX - winW / 2) / 30) * - 1;
-        // var posY = ((e.pageY - winH / 2) / 30) * - 1;
-
-        var posX = (e.pageX - winW / 2);
-        var posY = (e.pageY - winH / 2);
-        //console.log(posX, posY);
-
-        TweenMax.to($('.big_move'), moveSpeed, {
-            css: {
-                transform : "translate(" + (posX / bigMove) + "px, " + (posY / bigMove) + "px"
-            }
-        })
-
-        TweenMax.to($('.small_move'), moveSpeed, {
-            css: {
-                transform : "translate(" + (posX / smallMove) + "px, " + (posY / smallMove) + "px"
-            }
-        })
-
-        TweenMax.to($('.big_move_r'), moveSpeed, {
-            css: {
-                transform : "translate(" + (- posX / bigMove) + "px, " + (- posY / bigMove) + "px"
-            }
-        })
-
-        TweenMax.to($('.small_move_r'), moveSpeed, {
-            css: {
-                transform : "translate(" + (- posX / smallMove) + "px, " + (- posY / smallMove) + "px"
-            }
-        })
-
-        TweenMax.to($('.etc_move'), moveSpeed, {
-            css: {
-                transform : "translate(" + (posX / minMove) + "px, " + (posY / minMove) + "px"
-            }
-        })
-
-        TweenMax.to($('.etc_move_r'), moveSpeed, {
-            css: {
-                transform : "translate(" + (- posX / minMove) + "px, " + (- posY / minMove) + "px"
-            }
-        })
-
-
-        TweenMax.to($('.move_01'), moveSpeed, {
-            css: {
-                transform : "translate(" + (posX / move) + "px, " + (posY / move) + "px"
-            }
-        })
-
-        TweenMax.to($('.move_02'), moveSpeed, {
-            css: {
-                transform : "translate(" + (- posX / move) + "px, " + (- posY / move) + "px"
-            }
-        })
+};
 
 
 
-        // Text Mask Parallax
-
-        TweenMax.to($('.p_bg'), moveSpeed, {
-            css: {
-                backgroundPosition : ((posX / minMove) + 50) + "% " + ((posY / minMove) + 50) + "%"
-            }
-        })
-
-        TweenMax.to($('.link_mouse'), moveSpeed, {
-            css: {
-                backgroundPosition : ((posX / bigMove) + 50) + "% " + ((posY / bigMove) + 50) + "%"
-            }
-        })
-
-        TweenMax.to($('.img_box h1'), moveSpeed, {
-            css: {
-                backgroundPosition : ((posX / minMove) + 50) + "% " + ((posY / minMove) + 50) + "%"
-            }
-        })
-
-    });
 
 
+// Nice Scroll
 
-    $('.about .contents').mousemove(function(e) {
+$(document).ready(function (){
 
-        var posX = (e.pageX - winW / 2);
-        var posY = (e.pageY - winH / 2);
+	//$('html').niceScroll();
 
-        TweenMax.to($('.contents #color'), moveSpeed, {
-            css: {
-                transform : "translate(" + (- posX / bigMove) + "px, " + (- posY / bigMove) + "px"
-            }
-        })
-
-        TweenMax.to($('.text_box h1'), moveSpeed, {
-            css: {
-                transform : "translate(" + (posX / smallMove) + "px, " + (posY / smallMove) + "px"
-            }
-        })
-        
-    });
-
-    $('.profile').mousemove(function(e) {
-
-        var posX = (e.pageX - conW - proW / 2);
-        var posY = (e.pageY - winH / 2);
-        //console.log(posX, posY);
-
-        TweenMax.to($('#img'), moveSpeed, {
-            css: {
-                transform : "translate(" + (- posX / bigMove) + "px, " + (- posY / bigMove) + "px"
-            }
-        })
-
-        TweenMax.to($('.img_box h1'), moveSpeed, {
-            css: {
-                transform : "translate(" + (posX / smallMove) + "px, " + (posY / smallMove) + "px"
-            }
-        })
-
-    });
-
-
+	$('html').niceScroll({
+		zindex:100000,
+		scrollspeed:200,
+		mousescrollstep:20,
+		cursorcolor:"#5f6245",
+		cursoropacitymin:0,
+		cursoropacitymax:1,
+		cursorwidth:"8px",
+		cursorminheight:100,
+		cursorborder:"0px solid #fff",
+		cursorborderradius:"4px",
+		background:"",
+		hidecursordelay:1000
+	});
 });
 
 
 
 
 
-// Main Title
+// Header Show
 
-$('.title_wrap').on('mouseover', goSub)
-$('.title_wrap').on('mouseout', noSub)
+var scrollTop = $(window).scrollTop();
 
+$(window).scroll(function(){
 
+	var curTop = $(window).scrollTop();
 
-function goSub(){
-	//console.log("Go Sub Page");
+	if(curTop > scrollTop){
+		// Scroll Down
+		$('header').css('top',-40);
+		$('#hamburger').css('top',-40);
+	}else{
+		// Scroll Up
+		$('header').css('top',20);
+		$('#hamburger').css('top',20);
+	}
 
-	$('.main_title').show().stop(true).delay(200)
-    .animate({'opacity':0}, 500)
-
-	$('.link_title').show().stop(true).delay(500)
-    .animate({'opacity':1}, 1000)
-} 
-
-function noSub(){
-	//console.log("No Sub Page");
-
-	$('.link_title').show().stop(true).delay(200)
-    .animate({'opacity':0}, 500)
-
-	$('.main_title').show().stop(true).delay(500)
-    .animate({'opacity':1}, 1000)
-} 
+	scrollTop = curTop;
+});
 
 
 
 
 
-// Main Title Mouseover
+// Menu Show
 
-$('.link_title a').on('mouseover', function(){
-    //console.log("mouseOn");
-    $(this).addClass('link_mouse');
-})
+var menuDur = 800;
 
-$('.link_title a').on('mouseout', function(){
-    $(this).removeClass('link_mouse');
-})
+$('#hamburger').on("click",function(e) {
+	e.preventDefault();
+	
+    //alert("click");
+
+    $('#menu_wrap').stop().animate({'right': '0'}, menuDur, 'easeInOutQuint');
+});
+
+$('#exit').on("click",function(e) {
+    //alert("click");
+    e.preventDefault();
+    $('#menu_wrap').stop().animate({'right': '-100%'}, menuDur, 'easeInOutQuint');
+});
 
 
 
+$('#menu_wrap').on('scroll touchmove mousewheel', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  return false;
+});
 
 
-// About Image Mouseover
 
-$('.profile').on('mouseover', zoomIn);
-$('.profile').on('mouseout', zoomOut);
+$('#menu ul#menu_list li').mouseenter(function(){
 
-function zoomIn(){
-	//console.log("Zoom Image");
+	//console.log("Mouse Enter");
 
-	$('#img img').stop(true).animate({
-        'height':'130%',
-        'marginTop' : - ($('#img img').height() / 2)
-    }, 500)
+	$(this).stop().animate({'margin-left': '20px'}, menuDur, 'easeOutBounce');
+	$(this).stop().animate({'margin-left': '30px'}, menuDur, 'easeOutBounce');
+});
 
-    $('.img_box h1').stop(true)
-    .animate({'opacity':0.5}, 1000, 'easeOutCubic')
+$('#menu ul#menu_list li').mouseleave(function(){
 
-}
+	//console.log("Mouse Leave");
 
-function zoomOut(){
-	//console.log("Zoom Image");
-
-	$('#img img').stop(true).animate({
-        'height':'135%'
-    }, 500)
-
-    $('.img_box h1').stop(true).delay(100)
-    .animate({'opacity':0.2}, 1000, 'easeOutCubic')
-}
+	$(this).stop().animate({'margin-left': '0'}, menuDur, 'easeOutBounce');
+	$(this).stop().animate({'margin-left': '0'}, menuDur, 'easeOutBounce');
+});
