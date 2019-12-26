f3746cac2cf0eef1d2927d8e295d5f6fc4972ecd
Dahye <ydh0606@naver.com>
2019-12-27 05:31:25 +0900

    20191226
    -

--- js/loader.js
+++ js/loader.js
@@ -10,89 +10,91 @@
 
 var dur = 1; 
 
-var titleLoad = setTimeout(function(){
+//$(document).ready(function() {
 
-	// Header
+	var titleLoad = setTimeout(function(){
 
-	TweenMax.set($('header ul'), {y:-5});
-	TweenMax.to($('header ul'), dur, {y:0});
+		// Header
 
-	$('header ul').show().stop(true).delay(0)
-    .animate({'opacity':1}, 500)
+		TweenMax.set($('header ul'), {y:-5});
+		TweenMax.to($('header ul'), dur, {y:0});
 
-    // Title
+		$('header ul').show().stop(true).delay(0)
+	    .animate({'opacity':1}, 500)
 
-	TweenMax.set($('#title_wrap h1'), {y:20});
-	TweenMax.to($('#title_wrap h1'), dur, {y:0, ease:Back.easeOut.config(5)});
-	TweenMax.set($('#title_wrap h2'), {y:20});
-	TweenMax.to($('#title_wrap h2'), dur, {y:0, ease:Back.easeOut.config(5)});
+	    // Title
 
-	$('#title_wrap h1').show().stop(true).delay(0)
-    .animate({'opacity':1}, 500)
-    $('#title_wrap h2').show().stop(true).delay(50)
-    .animate({'opacity':1}, 500);
+		TweenMax.set($('#title_wrap h1'), {y:20});
+		TweenMax.to($('#title_wrap h1'), dur, {y:0, ease:Back.easeOut.config(5)});
+		TweenMax.set($('#title_wrap h2'), {y:20});
+		TweenMax.to($('#title_wrap h2'), dur, {y:0, ease:Back.easeOut.config(5)});
 
-    // Wave
+		$('#title_wrap h1').show().stop(true).delay(0)
+	    .animate({'opacity':1}, 500)
+	    $('#title_wrap h2').show().stop(true).delay(50)
+	    .animate({'opacity':1}, 500);
 
-    TweenMax.set($('#section_cont.cont-i'), {y:40});
-	TweenMax.to($('#section_cont.cont-i'), dur, {y:0, ease:Back.easeOut.config(2)});
+	    // Wave
 
-	 $('#section_cont.cont-i').show().stop(true).delay(0)
-    .animate({'opacity':1}, 1000);
+	    TweenMax.set($('#section_cont.cont-i'), {y:40});
+		TweenMax.to($('#section_cont.cont-i'), dur, {y:0, ease:Back.easeOut.config(2)});
 
-    // Sub Content
+		 $('#section_cont.cont-i').show().stop(true).delay(0)
+	    .animate({'opacity':1}, 1000);
 
-    TweenMax.set($('.cont-s'), {y:20});
-	TweenMax.to($('.cont-s'), dur, {y:0, ease:Back.easeOut.config(2)});
+	    // Sub Content
 
-	$('.cont-s').stop(true).delay(0)
-    .animate({'opacity':1}, 1000);
+	    TweenMax.set($('.cont-s'), {y:20});
+		TweenMax.to($('.cont-s'), dur, {y:0, ease:Back.easeOut.config(2)});
 
-    $('#img_wrap').show().stop(true).delay(100)
-    .animate({'opacity':1}, 1000);
+		$('.cont-s').stop(true).delay(0)
+	    .animate({'opacity':1}, 1000);
 
-}, 10)
+	    $('#img_wrap').show().stop(true).delay(100)
+	    .animate({'opacity':1}, 1000);
+
+	}, 10)
 
 
-var shapeLoad = setTimeout(function(){
+	var shapeLoad = setTimeout(function(){
 
-	TweenMax.set($('.shape'), {scale:0.95});
-	TweenMax.to($('.shape'), dur, {scale:1});
+		TweenMax.set($('.shape'), {scale:0.95});
+		TweenMax.to($('.shape'), dur, {scale:1});
 
-	// Main Shape
+		// Main Shape
 
-	TweenMax.set($('#m_shape_green'), {x:'18%'});
-	TweenMax.to($('#m_shape_green'), 1, {x:'15%', ease:Circ.easeOut});
-	TweenMax.set($('#m_shape_pink'), {x:-20});
-	TweenMax.to($('#m_shape_pink'), 1, {x:0, ease:Circ.easeOut});
-	TweenMax.set($('#m_shape_yellow'), {x:20});
-	TweenMax.to($('#m_shape_yellow'), 1, {x:0, ease:Circ.easeOut});
+		TweenMax.set($('#m_shape_green'), {x:'18%'});
+		TweenMax.to($('#m_shape_green'), 1, {x:'15%', ease:Circ.easeOut});
+		TweenMax.set($('#m_shape_pink'), {x:-20});
+		TweenMax.to($('#m_shape_pink'), 1, {x:0, ease:Circ.easeOut});
+		TweenMax.set($('#m_shape_yellow'), {x:20});
+		TweenMax.to($('#m_shape_yellow'), 1, {x:0, ease:Circ.easeOut});
 
-	$('#m_shape_green').show().stop(true).delay(80)
-    .animate({'opacity':1}, 500);
-    $('#m_shape_pink').show().stop(true).delay(0)
-    .animate({'opacity':1}, 500);
-    $('#m_shape_yellow').show().stop(true).delay(40)
-    .animate({'opacity':1}, 500);
+		$('#m_shape_green').show().stop(true).delay(80)
+	    .animate({'opacity':1}, 500);
+	    $('#m_shape_pink').show().stop(true).delay(0)
+	    .animate({'opacity':1}, 500);
+	    $('#m_shape_yellow').show().stop(true).delay(40)
+	    .animate({'opacity':1}, 500);
 
-    // About Shape
+	    // About Shape
 
-    TweenMax.set($('#a_shape_green'), {x:20});
-	TweenMax.to($('#a_shape_green'), 1, {x:0, ease:Circ.easeOut});
-	TweenMax.set($('#a_shape_pink'), {x:20});
-	TweenMax.to($('#a_shape_pink'), 1, {x:0, ease:Circ.easeOut});
-	TweenMax.set($('#a_shape_yellow'), {x:-20});
-	TweenMax.to($('#a_shape_yellow'), 1, {x:0, ease:Circ.easeOut});
+	    TweenMax.set($('#a_shape_green'), {x:20});
+		TweenMax.to($('#a_shape_green'), 1, {x:0, ease:Circ.easeOut});
+		TweenMax.set($('#a_shape_pink'), {x:20});
+		TweenMax.to($('#a_shape_pink'), 1, {x:0, ease:Circ.easeOut});
+		TweenMax.set($('#a_shape_yellow'), {x:-20});
+		TweenMax.to($('#a_shape_yellow'), 1, {x:0, ease:Circ.easeOut});
 
-	$('#a_shape_green').show().stop(true).delay(80)
-    .animate({'opacity':1}, 500);
-    $('#a_shape_pink').show().stop(true).delay(0)
-    .animate({'opacity':1}, 500);
-    $('#a_shape_yellow').show().stop(true).delay(40)
-    .animate({'opacity':1}, 500);
+		$('#a_shape_green').show().stop(true).delay(80)
+	    .animate({'opacity':1}, 500);
+	    $('#a_shape_pink').show().stop(true).delay(0)
+	    .animate({'opacity':1}, 500);
+	    $('#a_shape_yellow').show().stop(true).delay(40)
+	    .animate({'opacity':1}, 500);
 
-}, 100)
-
+	}, 100)
+//});
 
 
 
